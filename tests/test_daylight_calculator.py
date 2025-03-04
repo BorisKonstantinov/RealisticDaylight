c80607884dffe45ad585b4d1fcbace3dde5e1cef
Boris Konstantinov <b-ivanov@outlook.com>
2025-03-04 08:38:41 +0000

    Introduce pytest for testing

---
+++ tests/test_daylight_calculator.py
@@ -0,0 +1,13 @@
+import pytest
+
+from src.daylight_calculator import DaylightCalculator
+
+
+def test_nautical_exposure():
+    # TODO: Implement a test for nautical_exposure method
+    pass
+
+
+def test_realistic_daylight():
+    # TODO: Implement a test for realistic_daylight method
+    pass
---
+++ tests/test_orography_handler.py
@@ -0,0 +1,8 @@
+import pytest
+
+from src.orography_handler import OrographyHandler
+
+
+def test_get_orography_data():
+    # TODO: Implement a test for get_orography_data method
+    pass
